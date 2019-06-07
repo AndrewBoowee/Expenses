192e8569ab3329cab0f492b57e6b6f31a13efe73
AndrewBoowee <40044728+AndrewBoowee@users.noreply.github.com>
2019-06-07 03:12:36 -0500

    Add files via upload

---
+++ Expenses/Expenses/Expense.swift
@@ -0,0 +1,16 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Drew Boowee on 6/7/19.
+//  Copyright © 2019 Drew Boowee. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
+
